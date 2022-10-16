fc76572ed07622dcd29d20515110a7e80fe1c317
Summira Hussain <summira508411@gmail.com>
2022-10-16 21:21:35 +0100

    work

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import './navbar.css'
+import React from 'react';
+import './navbar.css';
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
 
 const Navbar = () => {
   return (
-    <div>navbar</div>
+    <div className='spc__navbar'>
+      <div className='spc__navbar-links'>
+        <div className='spc__navbar-links_logo'>
+          {/* <img src={logo} /> */}
+
+        </div>
+      </div>
+    </div>
   )
 }
 
